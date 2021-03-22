82d465597f89422aaee2992e0c0bba012af4c2e6
egemen6k <egemen.altikardesler@gmail.com>
2021-03-22 20:19:15 +0300

    CharacterMovemenet
    +Horizontal Movement
    +Forward movement
    +Road

---
+++ Assets/Scripts/Player/Scripts/InputPC.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputPC : MonoBehaviour,IInput
+{
+    public int GetHorizontalInput(int lane)
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            lane++;
+            if (lane > 2)
+            {
+                lane = 2;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            lane--;
+            if (lane < 0)
+            {
+                lane = 0;
+            }
+        }
+        return lane;
+    }
+
+    public void GetJumpInput()
+    {
+        Debug.Log("Jump");
+    }
+
+    public void GetSlideInput()
+    {
+        Debug.Log("Slide");
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # HC-Run
+
+-----
+22.03.2021
++Horizontal lane swapping function
++Forward movement
++Road tile
+---First commit
+
+
